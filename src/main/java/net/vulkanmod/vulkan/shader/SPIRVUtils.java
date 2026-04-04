6e93dd2c021585218ffb456752177b78bbbc33b9
denotas <denotasgomes@gmail.com>
2026-04-04 12:33:18 +0200

    Update SPIRVUtils.java

--- src/main/java/net/vulkanmod/vulkan/shader/SPIRVUtils.java
+++ src/main/java/net/vulkanmod/vulkan/shader/SPIRVUtils.java
@@ -7,7 +7,7 @@ import org.lwjgl.system.NativeResource;
 import org.lwjgl.util.shaderc.ShadercIncludeResolveI;
 import org.lwjgl.util.shaderc.ShadercIncludeResult;
 import org.lwjgl.util.shaderc.ShadercIncludeResultReleaseI;
-import org.lwjgl.vulkan.VK12;
+import org.lwjgl.vulkan.VK11;
 
 import java.io.IOException;
 import java.net.URI;
@@ -61,7 +61,7 @@ public class SPIRVUtils {
         if (DEBUG)
             shaderc_compile_options_set_generate_debug_info(options);
 
-        shaderc_compile_options_set_target_env(options, shaderc_env_version_vulkan_1_2, VK12.VK_API_VERSION_1_2);
+        shaderc_compile_options_set_target_env(options, shaderc_env_version_vulkan_1_1, VK11.VK_API_VERSION_1_1);
         shaderc_compile_options_set_include_callbacks(options, SHADER_INCLUDER, SHADER_RELEASER, pUserData);
 
         includePaths = new ObjectArrayList<>();
